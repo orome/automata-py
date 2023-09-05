248dc3217cb945ac6bce44bfb39acaa76498d763
Roy Levien <royl@msn.com>
2023-09-05 14:15:48 -0400

    Clean out old Turing code fron test stups

--- tests/test_stub.py
+++ tests/test_stub.py
@@ -5,9 +5,5 @@ import sys
 import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
 
-from machine import TuringMachine, Table, R, L, N, E, Behavior
-from utils import *
-
-
 def test_placeholder() -> None:
     pass

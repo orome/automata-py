ce9597ef28d58f1ece4bbe1d3e1468ad7b839f72
Roy Levien <royl@msn.com>
2023-09-10 16:14:58 -0400

    Improve default bounds handling

--- automata.py
+++ automata.py
@@ -18,7 +18,9 @@ class CellularAutomataError(ValueError):
 
 
 class CellularAutomata:
-    def __init__(self, rule_number, initial_conditions, frame_width=101, frame_steps=200, boundary_condition="zero"):
+    def __init__(self, rule_number, initial_conditions,
+                 frame_width=101, frame_steps=200,
+                 boundary_condition="zero"):
         # Validate the rule number
         if rule_number < 0 or rule_number > 255:
             raise CellularAutomataError("Invalid rule number. Must be between 0 and 255.")
@@ -45,6 +47,7 @@ class CellularAutomata:
         # Set the initial conditions
         center = self.frame_width // 2
         for idx in initial_conditions:
+            # Check that all nonzero initial conditions are within the frame
             if center + idx < 0 or center + idx >= self.frame_width:
                 raise CellularAutomataError("Initial condition at index", str(idx), "is outside of the frame.")
             self._lattice[0, center + idx] = 1
@@ -53,11 +56,15 @@ class CellularAutomata:
         self._compute_automaton()
 
     def _rule_number_to_binary(self, rule_number):
-        """Converts a rule number to its binary representation."""
+        """
+        Converts a rule number to its binary representation.
+        """
         return format(rule_number, '08b')
 
     def _get_boundary_values(self, current_row):
-        """Returns boundary values based on boundary condition and current row."""
+        """
+        Returns boundary values based on boundary condition and current row.
+        """
         if self.boundary_condition == "zero":
             return (0, 0)
         elif self.boundary_condition == "periodic":
@@ -66,7 +73,9 @@ class CellularAutomata:
             return (1, 1)
 
     def _compute_automaton(self):
-        """Generates the automaton based on the rule and initial conditions."""
+        """
+        Generates the automaton based on the rule and initial conditions.
+        """
         for row in range(1, self.frame_steps):
             left_boundary, right_boundary = self._get_boundary_values(self._lattice[row - 1])
             extended_current_row = np.hstack(([left_boundary], self._lattice[row - 1], [right_boundary]))
@@ -76,42 +85,62 @@ class CellularAutomata:
                 self._lattice[row, col - 1] = int(self.rule_binary[idx])  # Adjusted to account for extended row
 
     def _check_highlight_bounds(self, highlight_start_step, highlight_steps, highlight_width, highlight_offset):
-        """Checks bounds for highlighting and returns error messages if they exist."""
+        """
+        Checks bounds for highlighting and returns error messages any explicitly provided.
+        """
         error_messages = []
-        if highlight_start_step + highlight_steps > self.frame_steps:
-            error_messages.append(
-                f"starts at step {highlight_start_step} and ends at step {highlight_start_step + highlight_steps} (> {self.frame_steps})")
-        if highlight_start_step < 0:
-            error_messages.append(f"starts before step 0")
-        if self.frame_width // 2 + highlight_offset + highlight_width // 2 > self.frame_width:
-            error_messages.append(
-                f"width exceeds right bound with {self.frame_width // 2 + highlight_offset + highlight_width // 2} (> {self.frame_width})")
-        if self.frame_width // 2 + highlight_offset - highlight_width // 2 < 0:
-            error_messages.append(
-                f"width exceeds left bound with {self.frame_width // 2 + highlight_offset - highlight_width // 2} (< 0)")
+        if all([highlight_start_step, highlight_steps]):
+            if highlight_start_step + highlight_steps > self.frame_steps:
+                error_messages.append(
+                    f"starts at step {highlight_start_step} and ends at step {highlight_start_step + highlight_steps} (> {self.frame_steps})")
+        if all([highlight_start_step]):
+            if highlight_start_step < 0:
+                error_messages.append(f"starts before step 0")
+        if all([highlight_offset, highlight_width]):
+            if self.frame_width // 2 + highlight_offset + highlight_width // 2 > self.frame_width:
+                error_messages.append(
+                    f"width exceeds right bound with {self.frame_width // 2 + highlight_offset + highlight_width // 2} (> {self.frame_width})")
+            if self.frame_width // 2 + highlight_offset - highlight_width // 2 < 0:
+                error_messages.append(
+                    f"width exceeds left bound with {self.frame_width // 2 + highlight_offset - highlight_width // 2} (< 0)")
         return error_messages
 
-    def get_display(self, fig_width=12, highlight_width=None, highlight_steps=None,
-                highlight_start_step=0, highlight_offset=0, highlight_mask=0.3,
-                grid_color=None, grid_width=0.5):
-        """Displays the cellular automaton lattice optionally highlighting a frame within it and optionally showing a grid around the cells."""
+    def get_display(self, fig_width=12,
+                    highlight_start_step=None, highlight_steps=None, highlight_offset=None, highlight_width=None,
+                    highlight_mask=0.3, grid_color=None, grid_width=0.5,
+                    check_highlight_bounds=True):
+        """
+        Displays the cellular automaton lattice
+        optionally highlighting a frame within it and
+        optionally showing a grid around the cells.
+        """
 
-        if highlight_width is None:
-            highlight_width = self.frame_width - 2 * abs(highlight_offset) # FIX - Why times 2? <<<
+        # Check if the highlight region specified by any provided highlight bounds exceeds the bounds of the lattice
+        if check_highlight_bounds:
+            error_messages = self._check_highlight_bounds(highlight_start_step, highlight_steps,
+                                                          highlight_width, highlight_offset)
+            if error_messages:
+                raise CellularAutomataError(*error_messages)
+        print(highlight_start_step, highlight_steps, highlight_offset, highlight_width)
+
+        # Set any unspecified highlight bounds to default values
+        if highlight_start_step is None:
+            highlight_start_step = 0
         if highlight_steps is None:
             highlight_steps = self.frame_steps - highlight_start_step
+        if highlight_offset is None:
+            highlight_offset = 0
+        if highlight_width is None:
+            highlight_width = self.frame_width
 
-        # Check if the highlight region exceeds the bounds of the lattice
-        error_messages = self._check_highlight_bounds(highlight_start_step, highlight_steps, highlight_width,
-                                                      highlight_offset)
-        if error_messages:
-            raise CellularAutomataError(*error_messages)
+        print(highlight_start_step, highlight_steps, highlight_offset, highlight_width)
 
-        # Create a mask for highlighting
+        # Create a mask for highlighting, constraining the highlighted region to the bounds of the lattice
+        # !!! - Something wrong where with how frame with is fixed <<<
         mask = np.ones_like(self._lattice) * highlight_mask
         mask[highlight_start_step:highlight_start_step + highlight_steps,
-        self.frame_width // 2 + highlight_offset - highlight_width // 2: # FIX - Assumes symmetryc which isn't true if shifted! <<<
-        self.frame_width // 2 + highlight_offset + highlight_width // 2] = 1
+        self.frame_width // 2 + min(0, highlight_offset - highlight_width // 2):
+        self.frame_width // 2 + max(0, highlight_offset + highlight_width // 2)] = 1
 
         # Plotting
         fig, ax = plt.subplots(figsize=(fig_width, fig_width * self.frame_steps / self.frame_width))

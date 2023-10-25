aa2cebe80f744f6e79cc516b547d2aa0343f7af8
Roy Levien <royl@msn.com>
2023-10-25 16:30:16 -0400

    Tidy

--- automata/__init__.py
+++ automata/__init__.py
@@ -11,10 +11,10 @@ Description
 # See - http://www.python.org/dev/peps/pep-0440/
 __author__ = 'Roy Levien'
 __release__ = '0.1'  # N(.N)*
-__pre_release__ = 'a1'  # aN | bN | cN |
+__pre_release__ = 'a2'  # aN | bN | cN |
 __suffix__ = '.dev001'  # .devN | | .postN
 __version__ = __release__ + __pre_release__ + __suffix__
 
 from .core import *
 
-# TBD - Add doc comment sand package notes to this file <<<
+# TBD - Add doc comments and package notes to this file <<<

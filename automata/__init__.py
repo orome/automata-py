e776a8ac3f38f9b17f7ac89f8f5ca8593528ab5f
Roy Levien <royl@msn.com>
2023-11-02 11:09:39 -0400

    Improve publishing logic and combine with tests

--- automata/__init__.py
+++ automata/__init__.py
@@ -1,16 +1,21 @@
 """
-A brief description of the automata package.
+A simple Python (3.10+) implementation aimed at simulating some cellular automata,
+primarily those focused on by Stephan Wolfram in his book "A New Kind of Science".
 
-A longer description of the automata package.
+Functionality is currently limited to 1D, multi-state, immediate neighbor automata,
+using various boundary conditions.
 """
 
+# USE - Not needed if specified in pyproject.toml
+# __author__ = 'Roy Levien' ;
+
 # See - http://www.python.org/dev/peps/pep-0440/
-# __author__ = 'Roy Levien'
+# USE -
 __release__ = '0.1'  # N(.N)*
-__pre_release__ = 'a3'  # aN | bN | cN |
-__suffix__ = '.dev013'  # .devN | | .postN
+# USE - To indicate status of release, can be on any branch
+__pre_release__ = 'a3'  # aN | bN | rcN
+# USE - For all commits on develop branch, never on main branch, increment after each commit (that publishes)
+__suffix__ = '.dev15'  # .devN |  (.postN)
 __version__ = __release__ + __pre_release__ + __suffix__
 
 from .core import *
-
-# TBD - Add doc comments and package notes to this file <<<

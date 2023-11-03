0495186f63d255f1ebaa26eb13827415e61935c3
Roy Levien <royl@msn.com>
2023-11-03 16:55:52 -0400

    Improve version and tag handling in GitHub Action

--- automata/__init__.py
+++ automata/__init__.py
@@ -8,12 +8,13 @@ Functionality is currently limited to 1D, multi-state, immediate neighbor automa
 # __author__ = 'Roy Levien' ;
 
 # See - http://www.python.org/dev/peps/pep-0440/
+# ^v(0|[1-9]\d*)\.(0|[1-9]\d*)(?:\.(0|[1-9]\d+))?((?:[ab]|rc)(0|[1-9]\d*))?(?:\.(?:dev|post)(0|[1-9]\d*))?$
 # USE -
 __release__ = '0.1'  # N(.N)*
 # USE - To indicate status of release, can be on any branch
 __pre_release__ = 'a3'  # aN | bN | rcN
 # USE - For all commits on develop branch, never on main branch, increment after each commit (that publishes)
-__suffix__ = '.dev18'  # .devN |  (.postN)
+__suffix__ = '.dev19'  # .devN |  (.postN)
 __version__ = __release__ + __pre_release__ + __suffix__
 
 from .core import *

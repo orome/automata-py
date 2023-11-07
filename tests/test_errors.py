cd78d6bc2de86ccb8e9e67318c0d329c27f6c7b0
Roy Levien <royl@msn.com>
2023-11-07 09:02:59 -0500

    Refactor handling of tests for errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,15 +1,12 @@
+import pytest
 from automata import *
 
 
 def test_invalid_rule_number() -> None:
 
     def _test_invalid_rule_number(rule_number: int, base: int) -> None:
-        try:
+        with pytest.raises(CellularAutomataError):
             Rule(rule_number, base=base)
-        except CellularAutomataError:
-            assert True
-        else:
-            assert False, "Expected a CellularAutomataError but didn't get one."
 
     _test_invalid_rule_number(512, base=2)
     _test_invalid_rule_number(-1, base=3)
@@ -19,13 +16,8 @@ def test_invalid_rule_number() -> None:
 def test_invalid_boundary_conditions() -> None:
 
     def _test_invalid_boundary_conditions(rule_number: int, boundary_condition) -> None:
-        try:
+        with pytest.raises(CellularAutomataError):
             CellularAutomata(rule_number, '1', boundary_condition=boundary_condition)
-        except CellularAutomataError:
-            assert True
-        else:
-            assert False, (f"Expected a CellularAutomataError but didn't get one for"
-                           f" boundary_condition={boundary_condition}.")
 
     _test_invalid_boundary_conditions(30, "invalid_boundary")
     _test_invalid_boundary_conditions(30, 5)
@@ -33,27 +25,15 @@ def test_invalid_boundary_conditions() -> None:
 
 
 def test_invalid_initial_conditions() -> None:
-    try:
+    with pytest.raises(CellularAutomataError):
         CellularAutomata(30, '1002', base=2)  # '2' is invalid for base 2
-    except CellularAutomataError:
-        assert True
-    else:
-        assert False, "Expected a CellularAutomataError but didn't get one."
 
 
 def test_invalid_base_in_encode() -> None:
-    try:
+    with pytest.raises(ValueError, match="Can't handle base"):
         Rule._encode(30, base=40, length=8)
-    except ValueError:
-        assert True
-    else:
-        assert False, "Expected a ValueError but didn't get one."
 
 
 def test_invalid_base_in_decode() -> None:
-    try:
+    with pytest.raises(ValueError):
         Rule._decode('00011110', base=40)
-    except ValueError:
-        assert True
-    else:
-        assert False, "Expected a ValueError but didn't get one."
